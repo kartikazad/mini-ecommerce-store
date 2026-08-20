69b640ffd7710550968a04c0d469fd1d959ef8ae
Kartik Azad <kartikazad421@gmail.com>
2026-08-20 22:05:27 +0530

    MongoDB in lib, env(string) & api

---
+++ app/api/products/route.js
@@ -0,0 +1,15 @@
+import connectDB from "@/lib/mongodb";
+import Product from "@/models/Product";
+
+export async function GET() {
+  await connectDB();
+  const products = await Product.find({});
+  return Response.json(products);
+}
+
+export async function POST(request) {
+  await connectDB();
+  const body = await request.json();
+  const product = await Product.create(body);
+  return Response.json(product);
+}
