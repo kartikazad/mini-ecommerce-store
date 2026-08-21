7107dff7e706e9e49584b77bea9909b20176aba4
kartikazad <kartikazad421@gmail.com>
2026-08-21 14:43:28 +0530

    Changed images

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -8,9 +8,9 @@ export async function GET() {
 
   const products = await Product.insertMany([
     { name: "Blue T-Shirt", price: 19.99, image: "https://i.ibb.co/39zXxqYp/Blue-tshirt.png" },
-    { name: "Red Sneakers", price: 49.99, image: "https://loremflickr.com/400/300/sneakers,shoes" },
+    { name: "Red Sneakers", price: 49.99, image: "https://i.ibb.co/LdF1Tp6N/red-sneakers.png" },
     { name: "Black Backpack", price: 34.99, image: "https://i.ibb.co/1JQHFJyL/lenovo-black-bag.png" },
-    { name: "Leather Wallet", price: 24.99, image: "https://loremflickr.com/400/300/wallet,leather" },
+    { name: "Leather Wallet", price: 24.99, image: "https://i.ibb.co/wFwj026J/leather-wallet.png" },
   ]);
 
   return Response.json({ message: "Seeded!", products });
